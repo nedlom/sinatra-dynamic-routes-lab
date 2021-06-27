c98acc26908e500cc23068634e10d586719ef530
Nick <nelom3@gmail.com>
2021-06-27 13:40:31 -0700

    commit tree e411bda67fb5fe66f3bde9399cfb7ea65ede1c0b

--- app.rb
+++ app.rb
@@ -3,4 +3,32 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/reversename/:name" do 
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get "/square/:number" do 
+    @number = params[:number]
+    "#{@number.to_i ** 2}"
+  end
+
+  get "/say/:number/:phrase" do
+    "#{params[:phrase] * params[:number].to_i}"
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    params.values.join(" ") + "."
+  end
+
+  get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
+    operations = {
+      "add" => "+",
+      "subtract" => "-",
+      "multiply" => "*",
+      "divide" => "/"}
+    "#{params[:number1].to_i.public_send operations[@operation], params[:number2].to_i}" 
+  end
+
 end
